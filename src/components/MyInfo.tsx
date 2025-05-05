4c999abbedd1888ba68374d83bac78fa254aa21f
eepy <eepy2.23@gmail.com>
2025-05-05 17:52:04 +0900

    💄 Update: MyInfo

--- src/components/MyInfo.tsx
+++ src/components/MyInfo.tsx
@@ -1,8 +1,8 @@
 export function MyInfo() {
   return (
-    <>
-      <h1 className="text-2xl font-medium">신명희</h1>
-      <h2 className="">프론트엔드 개발자</h2>
-    </>
+    <div className="">
+      <h1 className="text-4xl font-medium">신명희</h1>
+      <h2 className="text-xl font-light">프론트엔드 개발자</h2>
+    </div>
   );
 }
