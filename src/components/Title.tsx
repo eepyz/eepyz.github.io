8c91cebed74c23264f482c7f0a25692aa416f0f2
eepy <eepy2.23@gmail.com>
2025-05-06 05:05:33 +0900

    💄 Fix: title 스타일 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,9 +3,9 @@ interface TitleProps {
 }
 export function Title({ title }: TitleProps) {
   return (
-    <div>
-      <div>{title}</div>
-      <div></div>
+    <div className="mt-10 flex justify-center flex-col items-center">
+      <div className="text-2xl font-light">{title}</div>
+      <div className="w-full border"></div>
     </div>
   );
 }
