2a59c05bc43cc72ac56623f68092811c2d80c11f
eepy <eepy2.23@gmail.com>
2025-05-06 06:18:54 +0900

    🩹 Fix: 이미지 경로 수정

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,7 +1,8 @@
+import profileImg from '../assets/img/profile.jpg';
 export function ProfileImage() {
   return (
     <div className="shrink-0 w-40 h-40 overflow-hidden rounded-full">
-      <img className="object-cover" src="/src/assets/img/profile.jpg" alt="profile" />
+      <img className="object-cover" src={profileImg} alt="profile" />
     </div>
   );
 }
